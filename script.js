65e226d09b2dd5aa944c72c86b5efc8e4bc38cfc
martinsavov <you@example.com>
2021-03-03 20:43:34 +0200

    dice working

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 'use strict';
 
 
-let test;
+document.querySelector('.btn--roll').addEventListener('click',function(){
+    let diceNumber = Math.trunc(Math.random() * 6) + 1;
+
+    document.querySelector('.dice').src = 'dice-' + diceNumber + '.png';
+
+    console.log(diceNumber);
+});
