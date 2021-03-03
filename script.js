6fa8a9cde9e203f007d500d7e439a14249bed4f9
martinsavov <you@example.com>
2021-03-03 22:24:22 +0200

    Change active player

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 'use strict';
 
+let activeUser = 0;
 
 document.querySelector('.btn--roll').addEventListener('click',function(){
     let diceNumber = Math.trunc(Math.random() * 6) + 1;
 
     document.querySelector('.dice').src = 'dice-' + diceNumber + '.png';
+    
+    if(activeUser === 0){
+        console.log('active user is 0');
+        document.querySelector('.player--0').classList.add('player--active');
+        document.querySelector('.player--1').classList.remove('player--active');
 
-    console.log(diceNumber);
+        if(diceNumber === 1){
+            activeUser = 1;
+        }
+    } else if(activeUser === 1){
+        console.log('active user is 1')
+        document.querySelector('.player--0').classList.remove('player--active');
+        document.querySelector('.player--1').classList.add('player--active');
+
+        if(diceNumber === 1){
+            activeUser = 0;
+            
+            
+        }
+    }
 });
